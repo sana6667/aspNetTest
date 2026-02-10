e7af8cb54932aac0d7b58d71818d7bd48db78e46
sana <sana@loc.com>
2026-02-10 18:39:33 +0200

    add workflows

--- Terraform/module/stage/iam-policy-test/main.tf
+++ Terraform/module/stage/iam-policy-test/main.tf
@@ -14,3 +14,18 @@ provider "aws" {
 module "iam_policy" {
     source = "../../iam-roles"
 }
+
+output "terraf_role" {
+    description = "Github Terraform role"
+    value = module.iam_policy.github_terraform_role_arn
+}
+
+output "github_deploy_role" {
+    description = "Github deploy role"
+    value = module.iam_policy.github_deploy_role_arn
+}
+
+output "oidc" {
+    description = "OIDC"
+    value = module.iam_policy.github_oidc_role_arn
+}
