930b2733cd4d76477ef5b0b833c6b4c3a0f999c3
sana <sana@loc.com>
2026-02-14 14:31:32 +0200

    new wf

--- Terraform/module/bastion/ssh-key.tf
+++ Terraform/module/bastion/ssh-key.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "my_ssh_key" {
     key_name = "bastion-ssh-key"
-    public_key = file(var.conf_bastion_host["path_ssh_key"])
+    public_key = file("${path.module}/bastion.pub")
     tags = {
         name = "baction-admin-key-ssh"
     }
